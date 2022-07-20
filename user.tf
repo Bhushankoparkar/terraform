2b6adb8d3efba03a689c86abb31d09cfb8f77474
Bhushankoparkar <101624593+Bhushankoparkar@users.noreply.github.com>
2022-07-20 23:55:35 +0530

    Create user.tf

---
+++ user.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_user" "iam_1" {
+  name = "bhu1"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_2" {
+  name = "bhu2"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_3" {
+  name = "bhu3"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_4" {
+  name = "bhu4"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_5" {
+  name = "bhu5"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_6" {
+  name = "bhu6"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_7" {
+  name = "bhu7"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_8" {
+  name = "bhu8"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_9" {
+  name = "bhu9"
+  path = "/"
+}
+
+resource "aws_iam_user" "iam_10" {
+  name = "bhu10"
+  path = "/"
+}
